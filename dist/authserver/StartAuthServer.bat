d9933b6fcd7674085463592e003ff37894329836
rootmp <info.gatses@gmail.com>
2025-07-10 16:04:57 +0300

    Рабочий dp

--- dist/authserver/StartAuthServer.bat
+++ dist/authserver/StartAuthServer.bat
@@ -1,24 +1,30 @@
 @echo off
 title L2Studio - Essence Vanguard (Auth Server)
+
+set "JAVA=C:\Program Files\Java\jdk-17.0.15\bin\java.exe"
+
 :start
 echo Starting L2Studio - AuthServer.
 echo.
-java -server -Dfile.encoding=UTF-8 -XX:+AlwaysActAsServerClassMachine -Xmx64m -cp config;../libs/* l2s.authserver.AuthServer
+"%JAVA%" -server -Dfile.encoding=UTF-8 -XX:+AlwaysActAsServerClassMachine -Xmx64m -cp config;../libs/* l2s.authserver.AuthServer
+
 if ERRORLEVEL 2 goto restart
 if ERRORLEVEL 1 goto error
 goto end
+
 :restart
 echo.
 echo Server restarted ...
 echo.
 goto start
+
 :error
 echo.
-echo Server terminated abnormaly ...
+echo Server terminated abnormally ...
 echo.
+
 :end
 echo.
 echo Server terminated ...
 echo.
-
 pause
--- dist/gameserver/StartGameServer.bat
+++ dist/gameserver/StartGameServer.bat
@@ -1,25 +1,32 @@
 @echo off
 title L2Studio - Essence Vanguard (GameServer)
-SET MAX_USED_HEAP_IN_GB=6
-SET path="C:\Program Files\BellSoft\LibericaJDK-17\bin"
+
+REM Настройки
+set "MAX_USED_HEAP_IN_GB=6"
+set "JAVA=C:\Program Files\Java\jdk-17.0.15\bin\java.exe"
+
 :start
 echo Starting GameServer.
-if %MAX_USED_HEAP_IN_GB% gtr 2 java -server -Dfile.encoding=UTF-8 -Xmx%MAX_USED_HEAP_IN_GB%g -cp config;../libs/* l2s.gameserver.GameServer
+echo.
+"%JAVA%" -server -Dfile.encoding=UTF-8 -XX:+AlwaysActAsServerClassMachine -Xmx%MAX_USED_HEAP_IN_GB%g -cp config;../libs/* l2s.gameserver.GameServer
+
 if ERRORLEVEL 2 goto restart
 if ERRORLEVEL 1 goto error
 goto end
+
 :restart
 echo.
 echo Server restarted ...
 echo.
 goto start
+
 :error
 echo.
-echo Server terminated abnormaly ...
+echo Server terminated abnormally ...
 echo.
+
 :end
 echo.
 echo Server terminated ...
 echo.
-
 pause
